48f2b45bd3e1a228f10ea9f187724d35a4ef0b24
ryallmeida <ryallmeida@gmail.com>
2025-03-13 16:16:53 -0300

    glm_pca_2022::transversalidade

--- glm_pca_2022.R
+++ glm_pca_2022.R
@@ -2,41 +2,107 @@
 # SCRIPT::PRINCIPAL COMPONENTES ANALYSIS 
 # ===================================================================================
 
-Zscore_2022 <- read_excel("C://Users//Notebook//Desktop//R//TRABALHOFINAL//Zscore_PropriamenteDito//Zscore_2022.xlsx")
+# ---------------------------------------------
+# CARREGANDO PACOTES
+# ---------------------------------------------
+
+if(!require(pacman))
+  install.packages("pacman")  
+library(pacman)
+
+if(!require(openxlsx))
+  install.packages("openxlsx")  
+library(openxlsx)
+
+if(!require(tidyverse))
+  install.packages("tidyverse")  
+library(tidyverse)
+
+if(!require(readxl))
+  install.packages("readxl")  
+library(readxl)
+
+if(!require(corrgram))
+  install.packages("corrgram")  
+library(corrgram)
 
 # ---------------------------------------------
 
-model_2022 <- lm(IEGM_taxa_2021~., data=Zscore_2022)
-summary(model_2022)
+pacman::p_load(openxlsx, readxl, tidyverse, corrgram)
 
+# ---------------------------------------------
 
+# ===================================================================================
+# CARREGANDO DADOS
+# ===================================================================================
 
-# GENERALIZED LINEAR MODEL
-model_2022_gaussian <- glm(IEGM_taxa_2021~., data=Zscore_2022, family =  gaussian())
-summary(model_2022_gaussian)
+# NOTA -----------------------------------------------------------------------------
+# IMPORTANTE RESSALTAR QUE NESSA PARTE DO SCRIPT JÁ ESTAMOS TRABALHANDO COM OS DADOS PADRONIZADOS DO ZSCORE 
 
+# ---------------------------------------------
+# ANO: 2022
+# ---------------------------------------------
+
+Zscore_2022 <- read_excel("C://Users//Notebook//Desktop//R//TRABALHOFINAL//Zscore_PropriamenteDito/Zscore_2022.xlsx")
+
+summary(Zscore_2022)
+
+# VARIAVEL DEPENDENTE: INDICE DE EFETIVADE DA GESTÃO MUNICIPAL
+
+IEGM_2021 <- read_excel("C://Users//Notebook//Desktop//R//TRABALHOFINAL//Transversalidade//IEGM_Pernambuco_Manipulado//IEGM_PE_2021.xlsx")
+
+summary(IEGM_2021)
+
+# ===================================================================================
+# ANÁLISE DE CORRELAÇÃO
+# ===================================================================================
+
+corrplot::corrplot(Zscore_2022)
+
+# ALTERNATIVA AO CORRPLOT, DEVIDO OS DADOS DO ZSCORE NÃO ESTAREM ENTRE -1 E 1 
 library(corrgram)
 corrgram(Zscore_2022)
+# HÁ UM GRAU DE CORRELAÇÃO ENTRE ALGUMAS VARIÁVEIS
+# RESOLVEMOS ISSO A PARTIR DA TECNICA DE PRINCIPAL COMPONENT ANALYSIS 
 
-
+# ---------------------------------------------------
 pca_2022 <- prcomp(Zscore_2022, center = TRUE, scale. = TRUE)
 summary(pca_2022)
 
-explained_variance <- summary(pca_2022)$importance[3, ]
+# O SUMMARY DE PCA GERA TRÊS RESULTADOS 
+#(i)   DESVIO PADRÃO DOS COMPONENTES PRINCIPAIS 
+#(ii)  PROPORÇÃO DA VARIÂNCIA EXPLICADA POR CADA COMPONENTE 
+#(iii) VARIÂNCIA ACUMULADA, OU SEJA, A SOMA DAS PROPORÇÕES ATÉ CADA COMPONENE
+
+# EXTRAI APENAS A VARIÂNCIA ACUMULADA
+explained_variance <- summary(pca_2022)$importance[3, ] 
+print(explained_variance)
+
+# NUMERO MINIMO DE COMPONENTES PRINCIPAIS NECESSÁRIOS PARA EXPLICAR 95% DA VARIÂNCIA DOS DADOS 
 num_components <- which(cumsum(explained_variance) >= 0.95)[1]
+print(num_components)
 
 # ---------------------------------------------------
 
-
-# Obter os componentes principais
-X_pca <- pca_2022$x[, 1:num_components]
-
-# Criar um data frame com os componentes principais para usar no modelo
-X_pca_df <- as.data.frame(X_pca)
-
-# Construir o modelo de regressão usando os componentes principais
-model_pca <- lm(Zscore_2022$IEGM_taxa_2021 ~ -1 + PC1 + PC2 + PC3 + PC4, data = X_pca_df)
-summary(model_pca)
-
-
 pca_2022$rotation[1]
+
+# ===================================================================================
+# CONSTRUÇÃO DO MODELO 
+# ===================================================================================
+
+#OBTER COMPONENTES PRINCIPAIS 
+X_pca_2022 <- pca_2022$x[, 1:num_components]
+
+# CRIAR UM DATAFRAME COM OS COMPOMPONENTES PRINCIPAIS PARA USAR NO MODELO 
+# OU SEJA, DEVE-SE ISOLÁ-LOS
+X_pca_df_2022 <- as.data.frame(X_pca_2022)
+
+# CONSTRUIR O MODELO DE REGRESSÃO USANDO OS COMPENTENTES PRINCIPAIS
+model_pca_2022 <- lm(Zscore_2022$IEGM_taxa_2021 ~ -1 + PC1 + PC2 + PC3 + PC4, data = X_pca_df_2022)
+
+summary(model_pca_2022)
+
+# POR ALGUM MOTIVO O MODELO COM ESSA FUNÇÃO, O R^2 DEU 1, E COMO SABEMOS ISSO NAS CIÊNCIAS SOCIAIS APLICADAS É PRATICAMENTE IMPOSSÍVEL 
+
+#  --------------------------------------------------
+#  --------------------------------------------------
